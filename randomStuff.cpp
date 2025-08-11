5b3f23e694fa0e36e700e1cbf3bda0f4837ef4fa
iamjoel12 <joelhardware777@gmail.com>
2025-08-11 18:01:52 -0500

    Vectors and beginning of STL basics

--- randomStuff.cpp
+++ randomStuff.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+void printVector(std::vector<int>& v){
+    for(auto i: v){
+        std::cout << i << " " << std::endl;
+    }
+}
+/*my go to for printing vectors (best for STL logic)*/
+void printingVectors(std::vector<int> v){
+    for(auto it= v.begin(); it!=v.end(); ++it)
+    std::cout << *it << std::endl;
+}
 
 int main(){
-    int triangle[10][10];
-    int size = sizeof(triangle)/sizeof(triangle[0]);
 
-    for(int i=0; i=10; i++){
-        for(int j=0;j=10; j++){
-            std::cout << "*" << std::endl;
-        }
-    }
+    std::vector<int> vector1 = {2, 5, 6, 7, 9};
+    std::vector<int> vector2(5,9);
+
+    //print both vectors
+
+    printingVectors(vector1);
+    std::cout << "\n";
+    printingVectors(vector2);
+
     return 0;
 }
