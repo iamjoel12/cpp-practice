d8dc933c11528a9b614397804e3d9ad8065a7a88
iamjoel12 <joelhardware777@gmail.com>
2025-08-30 18:22:09 -0500

    OOPS Setters and Getters

--- OOPS.cpp
+++ OOPS.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 
-class Flowers{
-    public:
-    std::string name;
-    int numberOfPetals = 0;
+    class Person{
+        int socialID;
+        std::string name;
 
-    void grow(){
-        std::cout << "Number of petals is now " << numberOfPetals+2 << std::endl;
-    }
-    void newCharacteristics(){
-        std::cout << "New characteristics are assigned";
-    }
-    void color(){
-        std::cout << "The color is ";
-    }
+        public:
+        //contructer
+        Person(std::string n, int id) : name(n), socialID(id){}
+        
+        //Getter for name (const is used since it does not modify the object)
+        std::string getName() const{
+            return name;
+        }
 
-};
+        //Setter for name
+        void setName(std::string newName){
+            name = newName;
+        }
 
+        //Validate the social ID
+        bool validateID() const{
+            return (socialID >= 0 && socialID <= 1001);
+        }
+    };
 int main(){
-    Flowers Dandilion;
+    Person personOne("James", 503);
 
-    Dandilion.grow();  
+    if(!personOne.validateID()){
+        std::cout << "Invalid SocialID\n";
+    }else{
+        std::cout << "Name: " << personOne.getName() << std::endl;
+    }
+
+    return 0;
 }
